f4c2a0a3d3d040f53c0566b153dff022f4098544
WemmersonAlb <wemmerson.albuquerque@gmail.com>
2023-12-05 15:37:31 -0300

    Satisfeita todas as implementações necessárias e implementada a calidação de placa conforme regras do mercosul

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,9 +1,12 @@
+using System.Text.RegularExpressions;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
     {
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
+        private string placaFormatada;
         private List<string> veiculos = new List<string>();
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
@@ -14,9 +17,16 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
+            // DONE: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
-            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            Console.WriteLine("Digite a placa do veículo para estacionar: ");
+            string placa = Console.ReadLine();
+            if(placaIsValid(placa.ToUpper())){
+                veiculos.Add(placaFormatada);
+                Console.WriteLine("Veículo estacionado com sucesso.");
+            }else{
+                Console.WriteLine("Placa inválida.");
+            }
         }
 
         public void RemoverVeiculo()
@@ -25,23 +35,25 @@ namespace DesafioFundamentos.Models
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
-            string placa = "";
+            string placa = Console.ReadLine();
 
             // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            if (placaIsValid(placa.ToUpper()) && veiculos.Any(x => x.ToUpper() == placaFormatada.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
+                // DONE: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
+                // DONE: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal
                 // *IMPLEMENTE AQUI*
                 int horas = 0;
-                decimal valorTotal = 0; 
+                decimal valorTotal = 0;
+                horas = Convert.ToInt16(Console.ReadLine());
+                valorTotal = precoInicial + precoPorHora*horas;
 
-                // TODO: Remover a placa digitada da lista de veículos
+                // DONE: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
-
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                veiculos.Remove(placa.ToUpper());
+                Console.WriteLine($"O veículo {placaFormatada} foi removido e o preço total foi de: {valorTotal:c}");
             }
             else
             {
@@ -55,12 +67,58 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
+                // DONE: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
+                foreach(string veiculo in veiculos){
+                    Console.WriteLine(veiculo);
+                }
             }
             else
             {
                 Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+
+        public bool placaIsValid(string placa){
+            bool condition0 = placa.Length >= 7;
+
+            string letras = placa.Substring(0,3);
+            bool condition1 = Regex.IsMatch(letras, @"^[A-Z]+$");
+
+            string hifen = placa.Substring(3,1);
+            bool condition2 = Regex.IsMatch(hifen, @"^[-]+$");
+
+            bool condition3, condition4, condition5;
+            string numeroIsolado, numeroOuLetra, restanteDaPlaca;
+            if(condition2){ //Compatível com usuário colocando hífen ou não
+                numeroIsolado = placa.Substring(4,1);
+                condition3 = Regex.IsMatch(numeroIsolado, @"^[0-9]+$");
+
+                numeroOuLetra = placa.Substring(5,1); //Compatível com o novo padrão de placas mercosul
+                condition4 = Regex.IsMatch(numeroOuLetra, @"^[0-9ABCDEFGHIJ]+$");
+
+                restanteDaPlaca = placa.Substring(6);
+                condition5 = Regex.IsMatch(restanteDaPlaca, @"^[0-9]+$");
+            }else{
+                numeroIsolado = placa.Substring(3,1);
+                condition3 = Regex.IsMatch(numeroIsolado, @"^[0-9]+$");
+
+                numeroOuLetra = placa.Substring(4,1); //Compatível com o novo padrão de placas mercosul
+                condition4 = Regex.IsMatch(numeroOuLetra, @"^[0-9ABCDEFGHIJ]+$");
+
+                restanteDaPlaca = placa.Substring(5);
+                condition5 = Regex.IsMatch(restanteDaPlaca, @"^[0-9]+$");
+            }
+
+            if(condition0 &&
+                condition1 &&
+                condition3 &&
+                condition4 &&
+                condition5){
+                    this.placaFormatada = letras + " - " + numeroIsolado + numeroOuLetra + restanteDaPlaca;
+                    return true;
+            }else{
+                return false;
             }
         }
     }
